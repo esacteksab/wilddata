ce99f62d2d2e1ba0e66b9ed14c6cf0abe955cef2
Barry Morrison <b@rrymorrison.com>
2021-03-23 22:34:49 -0500

    adds svelte

---
+++ wtfizit-fe/src/Asset.svelte
@@ -0,0 +1,36 @@
+<script>
+    // create a prop
+    export let asset;
+</script>
+
+
+<foo>
+    <h1>Asset </h1>
+    <small>
+        Name: <b>{asset.Name}</b> <br/>
+        Org: <b>{asset.Org}</b> <br/>
+        {#if asset.Tags}
+        Tags: 
+            {#each asset.Tags as tag}
+            <ul>
+            <li>
+                <b>md5sum: </b>{tag.md5sum}</li>
+            
+            <li>
+                <b>sha256sum: </b>{tag.sha256sum}</li>
+            </ul>
+            {/each}
+            {/if}
+    </small><br/>
+</foo>
+
+<style>
+  foo {
+    margin: 0 0 1em 0;
+  }
+  h1 {
+    font-size: 1.4em;
+    margin: 0;
+    display: block;
+  }
+</style>
