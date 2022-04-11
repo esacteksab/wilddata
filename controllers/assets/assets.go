f6065d9c42671f7db5b8c74c469e9ac09d7dae4d
Barry Morrison <689591+esacteksab@users.noreply.github.com>
2022-04-10 19:29:30 -0500

    [wip]feat: get an asset by name, not id

--- controllers/assets/assets.go
+++ controllers/assets/assets.go
@@ -2,14 +2,13 @@ package assets
 
 import (
 	"fmt"
-	"strconv"
 
 	"github.com/esacteksab/wilddata/models"
 	"github.com/gin-gonic/gin"
 )
 
 // APIV1GetAssets gets all assets
-func APIV1GetAssets(c *gin.Context) {
+func APIV1GetAllAssets(c *gin.Context) {
 
 	db := models.InitDb()
 
@@ -31,28 +30,29 @@ func APIV1AddAsset(c *gin.Context) {
 	c.JSON(201, gin.H{"success": assets})
 }
 
-// APIV1GetAsset gets an individual asset
+// APIV1GetAsset gets all assets with name "name"
 func APIV1GetAsset(c *gin.Context) {
 
 	db := models.InitDb()
 
-	var asset models.Assets
-	id := c.Params.ByName("id")
+	var assets []models.Assets
+	db.Find(&assets)
+	name := c.Params.ByName("name")
 
-	// id above is a string, we need an int
-	sid, _ := strconv.Atoi(id)
+
 
 	// SELECT * from Assets where Org = `id`
-	db.Find(&asset, "id = ?", sid)
-	fmt.Println(asset)
-	c.JSON(200, asset)
+	db.Find(&assets, "name = ?", name)
+	fmt.Println(assets)
+	c.JSON(200, assets)
 }
 
 // APIV1UpdateAsset updates an individual asset
-func APIV1UpdateAsset(c *gin.Context) {
-	id := c.Param("id")
-	c.JSON(200, gin.H{"method": "PUT", "id": id})
-}
+// Assets are atomic. We don't want to allow updates
+// func APIV1UpdateAsset(c *gin.Context) {
+// 	id := c.Param("id")
+// 	c.JSON(200, gin.H{"method": "PUT", "id": id})
+// }
 
 // APIV1DeleteAsset deletes an individual asset
 func APIV1DeleteAsset(c *gin.Context) {
