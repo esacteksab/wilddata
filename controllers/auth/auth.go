bebcf22c62e729aa061dd5bd6c7840f8fef05c01
Barry Morrison <689591+esacteksab@users.noreply.github.com>
2022-06-19 13:39:57 -0500

    chore: WIP for the login function, but quite a bit of logic worked out and it now accepts JSON so a battle or two were won

--- controllers/auth/auth.go
+++ controllers/auth/auth.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/esacteksab/wilddata/models"
@@ -42,15 +43,37 @@ func APIV1AddUser(c *gin.Context) {
 	c.JSON(200, users)
 }
 
-
-
 // APIV1Login
 func APIV1Login(c *gin.Context) {
-	session := sessions.Default(c)
-	session.Set("name", c.Params.ByName("name"))
-	session.Save()
 
-	c.JSON(http.StatusOK, gin.H{"message": "User signed in..."})
+	db := models.InitDb()
+
+	var user models.Users
+
+	if c.ShouldBind(&user) == nil {
+		// Map
+		//db.Where(map[string]interface{}{"name": "jinzhu", "age": 20}).Find(&users)
+		// SELECT * FROM users WHERE name = "jinzhu" AND age = 20;
+		if result := db.Where(map[string]interface{}{"name": user.Name, "password": user.Password}).Find(&user); result.Error != nil {
+			c.String(200, "I am here")
+		} else {
+			if result.RowsAffected == 0 {
+				log.Println(result.RowsAffected)
+				c.JSON(http.StatusNotFound, gin.H{"session": "User not found"})
+			} else {
+				log.Println(result.RowsAffected)
+				log.Println(map[string]interface{}{"message": user.ID})
+				log.Println("SName: %s", user.Name)
+				log.Println("SPasswd: %s", user.Password)
+				c.JSON(http.StatusOK, gin.H{"message": "I found that user yo!"})
+
+			}
+		}
+
+	} else {
+		log.Fatal("You really fucked up! How did you get here?!")
+
+	}
 }
 
 // APIV1Logout
@@ -63,17 +86,17 @@ func APIV1Logout(c *gin.Context) {
 
 //AuthRequired ensures a session exists to access the endpoint
 func AuthRequired(c *gin.Context) {
-		session := sessions.Default(c)
-		sessionID := session.Get("name")
-		if sessionID == nil {
-			c.JSON(http.StatusForbidden, gin.H{
-				"message": "not authed",
-			})
-			c.Abort()
-		}
+	session := sessions.Default(c)
+	sessionID := session.Get("name")
+	if sessionID == nil {
+		c.JSON(http.StatusForbidden, gin.H{
+			"message": "not authed",
+		})
+		c.Abort()
 	}
+}
 
 // Ping will return Pong if a session exists
 func Ping(c *gin.Context) {
-	c.JSON(200, gin.H{"message": "pong",})
+	c.JSON(200, gin.H{"message": "pong"})
 }
